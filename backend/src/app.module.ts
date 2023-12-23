1fc3284efc0351fc5dfa2d25d7e40de9773b85de
Jack Chuma <jack@pixelvault.com>
2023-12-23 08:34:04 -0500

    add config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+      isGlobal: true,
+      load: [config],
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  port: parseInt(process.env.PORT) || 3000,
+  database: {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    name: process.env.DB_NAME,
+  },
+});
