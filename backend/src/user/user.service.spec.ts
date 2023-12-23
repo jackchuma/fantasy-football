2b911e78c227d92c3bc41a331e91118c6aa8930a
Jack Chuma <jack@pixelvault.com>
2023-12-23 09:09:34 -0500

    add testing for user service

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -23,4 +23,64 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('Create user', () => {
+    it('Should save user to db and return val', async () => {
+      const body = {
+        email: 'test@email.com',
+        password: 'pw',
+        firstName: 'bill',
+        lastName: 'brady',
+      };
+      const hashedPassword = 'hashed-pw';
+
+      const user = new User();
+      user.email = body.email;
+      user.password_hash = hashedPassword;
+      user.first_name = body.firstName;
+      user.last_name = body.lastName;
+      user.password_changed_at = expect.any(Date);
+
+      mockUserRepository.save = jest.fn();
+
+      expect(await service.createUser(body, hashedPassword)).toStrictEqual(
+        user,
+      );
+      expect(mockUserRepository.save).toHaveBeenCalledWith(user);
+    });
+  });
+
+  describe('Get User by email', () => {
+    it('Should query db based on email', async () => {
+      const email = 'test@email.com';
+
+      mockUserRepository.findOneBy = jest.fn();
+
+      await service.getUserByEmail(email);
+
+      expect(mockUserRepository.findOneBy).toHaveBeenCalledWith({ email });
+    });
+
+    it('Should return db response', async () => {
+      const email = 'test@email.com';
+      const user = { id: 'userId' };
+
+      mockUserRepository.findOneBy = jest.fn().mockResolvedValue(user);
+
+      expect(await service.getUserByEmail(email)).toStrictEqual(user);
+    });
+
+    it('Should catch db error and throw internal server error', async () => {
+      const email = 'test@email.com';
+
+      mockUserRepository.findOneBy = jest.fn().mockRejectedValue('error');
+
+      try {
+        await service.getUserByEmail(email);
+        expect(true).toBe(false);
+      } catch (e) {
+        expect(e.message).toBe('Internal Server Error');
+      }
+    });
+  });
 });
