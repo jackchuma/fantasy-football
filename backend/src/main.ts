8f2ff661d4ea937fa2d5e672964d23e91d935218
Jack Chuma <jack@pixelvault.com>
2023-12-23 08:57:50 -0500

    update main.ts file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow('port');
+
+  app.use(cookieParser());
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
+  app.setGlobalPrefix('/api/v1');
+
+  await app.listen(port);
+  console.log(`App listening on port ${port}`);
 }
 bootstrap();
