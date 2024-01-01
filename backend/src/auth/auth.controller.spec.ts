c1d2a2d9b90a80f2415a9d1bf18202eb4a6ee22a
Jack Chuma <jack@pixelvault.com>
2023-12-31 19:49:20 -0500

    add controller function for register user with testing

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let mockAuthService: any;
 
   beforeEach(async () => {
+    mockAuthService = {};
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +19,37 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Register User', () => {
+    it('Should register user', async () => {
+      const body = {
+        email: 'test@email.com',
+        password: 'pw',
+        firstName: 'name',
+        lastName: 'lastname',
+      };
+      const res: any = { type: 'response' };
+
+      mockAuthService.registerUser = jest.fn();
+
+      await controller.registerUser(body, res);
+      expect(mockAuthService.registerUser).toHaveBeenCalledWith(body, res);
+    });
+
+    it('Should return service response', async () => {
+      const body = {
+        email: 'test@email.com',
+        password: 'pw',
+        firstName: 'name',
+        lastName: 'lastname',
+      };
+      const res: any = { type: 'response' };
+      const resp = { success: true };
+
+      mockAuthService.registerUser = jest.fn().mockResolvedValue(resp);
+
+      expect(await controller.registerUser(body, res)).toStrictEqual(resp);
+    });
   });
 });
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async registerUser(
+    @Body() body: CreateUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return await this.authService.registerUser(body, res);
+  }
+}
